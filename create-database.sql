7221152730b85d48afbfc2efb3974b361179f4ed
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2020-12-06 23:00:47 -0500

    Update create-database.sql and add data files

--- create-database.sql
+++ create-database.sql
@@ -7,58 +7,73 @@ USE sass;
 
 CREATE TABLE DEPARTMENT (
 	dept_title		varchar(100) NOT NULL,
-    PRIMARY KEY		(dept_title)
+    dept_id			varchar(10) NOT NULL,
+    PRIMARY KEY		(dept_id)
 );
 
 CREATE TABLE PROGRAM (
 	program_title	varchar(100) NOT NULL,
-    dept_title		varchar(100) NOT NULL,
-    PRIMARY KEY		(program_title),
-    FOREIGN KEY		(dept_title) REFERENCES DEPARTMENT(dept_title)
+    degree_type		varchar(50) NOT NULL,
+    dept_id			varchar(10) NOT NULL,
+    PRIMARY KEY		(program_title, degree_type),
+    FOREIGN KEY		(dept_id) REFERENCES DEPARTMENT(dept_id)
 );
 
 CREATE TABLE STUDENT (
 	student_id 		int NOT NULL,
 	student_name 	varchar(100) NOT NULL,
     student_credits	int NOT NULL,
-    program_title	varchar(100) NOT NULL,
+    program_title	varchar(100),
+    degree_type		varchar(50),
     PRIMARY KEY 	(student_id),
-    FOREIGN KEY		(program_title) REFERENCES PROGRAM(program_title)
+    FOREIGN KEY		(program_title, degree_type) REFERENCES PROGRAM(program_title, degree_type)
 );
 
 CREATE TABLE FACULTY (
 	faculty_id		int NOT NULL,
     faculty_name	varchar(100) NOT NULL,
     faculty_email	varchar(100) NOT NULL,
-    dept_title		varchar(100) NOT NULL,
+    dept_id			varchar(10) NOT NULL,
     PRIMARY KEY		(faculty_id),
-    FOREIGN KEY		(dept_title) REFERENCES DEPARTMENT(dept_title)
+    FOREIGN KEY		(dept_id) REFERENCES DEPARTMENT(dept_id)
 );
 
 CREATE TABLE COURSE (
-	course_id		int NOT NULL,
+	course_id		varchar(50) NOT NULL,
     course_title	varchar(100) NOT NULL,
     course_credits	int NOT NULL,
     faculty_id		int NOT NULL,
-    dept_title		varchar(100) NOT NULL,
+    dept_id			varchar(10) NOT NULL,
     PRIMARY KEY		(course_id),
     FOREIGN KEY		(faculty_id) REFERENCES FACULTY(faculty_id),
-    FOREIGN KEY		(dept_title) REFERENCES DEPARTMENT(dept_title)
+    FOREIGN KEY		(dept_id) REFERENCES DEPARTMENT(dept_id)
 );
 
 CREATE TABLE REQUIREMENT (
 	requirement_name	varchar(100) NOT NULL,
     requirement_type	varchar(100) NOT NULL,
-    PRIMARY KEY			(requirement_name)
+    requirement_id		varchar(20) NOT NULL,
+    PRIMARY KEY			(requirement_id)
 );
 
 CREATE TABLE OBJECTIVE (
-	course_id			int NOT NULL,
-    requirement_name	varchar(100) NOT NULL,
-    program_title		varchar(100) NOT NULL,
+	course_id			varchar(50) NOT NULL,
+    requirement_id		varchar(20) NOT NULL,
+    program_title		varchar(100),
+    degree_type			varchar(50),
     PRIMARY KEY			(course_id),
     FOREIGN KEY			(course_id) REFERENCES COURSE(course_id),
-    FOREIGN KEY			(requirement_name) REFERENCES REQUIREMENT(requirement_name),
-    FOREIGN KEY			(program_title) REFERENCES PROGRAM(program_title)
+    FOREIGN KEY			(requirement_id) REFERENCES REQUIREMENT(requirement_id),
+    FOREIGN KEY			(program_title, degree_type) REFERENCES PROGRAM(program_title, degree_type)
+);
+
+CREATE TABLE HISTORY (
+	student_id 		int NOT NULL,
+    course_id		varchar(50) NOT NULL,
+    school_year		int NOT NULL,
+    semester		varchar(100) NOT NULL,
+    PRIMARY KEY		(student_id, course_id),
+	FOREIGN KEY		(student_id) REFERENCES STUDENT(student_id),
+	FOREIGN KEY		(course_id) REFERENCES COURSE(course_id)
 );
 
